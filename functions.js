e216c7c7d1c87c63470d32da380b09a94ae8cc8e
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-13 23:01:01 -0700

    Note deletion functionality

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
+var notesToDelete = new Array();
+
 function addNote(){
 	var notes = document.querySelector(".notes")
 	var newNote = document.querySelector(".noteTemplate").cloneNode(true)
@@ -20,6 +22,10 @@ function deleteNote(element){
 	var idNumber = element.id.match(/\d/g).join("")
 	var notes = document.querySelector(".notes")
 	var noteToDelete = document.querySelector("#" + element.id)
+	if(noteToDelete.name){
+		console.log("pushed to Delete")
+		notesToDelete.push(noteToDelete.name)
+	}
 	notes.removeChild(noteToDelete)
 }
 
@@ -28,12 +34,20 @@ function saveNotepad(){
 	var content = {};
 	var data = {};
 	var notes = document.querySelectorAll(".notes .myNotes")
-	notes.forEach(function(note, index){
-		content["content"] = note.querySelector(".noteBody").value
-		file[note.querySelector(".noteTitle").value + ".txt"] = content
-		data["files"] = file
-		postRequest("https://api.github.com/gists", data, note.id)
-	});
+	if(notes.length > 0){
+		notes.forEach(function(note, index){
+			content["content"] = note.querySelector(".noteBody").value
+			file[note.querySelector(".noteTitle").value + ".txt"] = content
+			data["files"] = file
+			postRequest("https://api.github.com/gists", data, note.id)
+		});
+	}
+	if(notesToDelete.length > 0){
+		notesToDelete.forEach(function(gistId){
+			deleteRequest(gistId)
+		})
+		notesToDelete.length = 0
+	}
 }
 
 function deleteNotepad(){
@@ -46,15 +60,27 @@ function postRequest(url, data, noteId){
 		body: JSON.stringify(data),
 		headers: new Headers({
 			'Content-Type': 'application/json',
-			'Authorization': 'token 39035ef83dc9e9140ed67c7b52b7beb28557dd92'
+			'Authorization': 'token 138337ec84788757f7fda1481a46908fed484838'
 		}),
 	})
 	.then(response => response.json())
 	.then(data => {
+		console.log(data)
 		var noteElement = document.querySelector("#note1")
-		noteElement.name = data.url
+		noteElement.name = data.id
 		if(noteElement.classList.contains("new"))
 			noteElement.classList.remove("new")
+	})
+	.catch(error => console.error(error))
+}
+
+function deleteRequest(gistId){
+	fetch(("https://api.github.com/gists/" + gistId), {
+		method: 'DELETE',
+		headers: new Headers({
+			'Content-Type': 'application/json',
+			'Authorization': 'token 138337ec84788757f7fda1481a46908fed484838'
+		}),
 	})
 	.catch(error => console.error(error))
 }
