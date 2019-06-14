dc62b67c2382bc954c89c680d165b7f20fcf6484
Marco Cornejo <alejandroc_89@hotmail.com>
2019-06-13 21:07:08 -0700

    Add note onclick()

---
+++ functions.js
@@ -0,0 +1,12 @@
+function addNote(){
+	var notes = document.querySelector(".notes")
+	var newNote = document.querySelector(".noteTemplate").cloneNode(true)
+	newNote.className = "myNotes"
+	newNote.classList.add("new")
+	var numberOfNotes = document.querySelectorAll(".notes .myNotes").length
+	newNote.id = "note" + (numberOfNotes + 1)
+	notes.appendChild(newNote)
+	document.querySelector("#" + newNote.id + " .delete").id = newNote.id
+	document.querySelector("#" + newNote.id + " .noteTitle").required = true
+	document.querySelector("#" + newNote.id + " .noteBody" ).required = true
+}
